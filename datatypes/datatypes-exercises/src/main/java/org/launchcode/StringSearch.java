83388ed5fa045884c5712c84b3f87b02ced1ccd4
Eric S <estriler@hotmail.com>
2024-09-07 13:54:03 -0500

    finished part e of exercise

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/StringSearch.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/StringSearch.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 
 public class StringSearch {
     public static void main(String[] args) {
-        String quote = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having " +
+        String originalQuote = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having " +
                 "nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures " +
                 "or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversation?’";
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a word or phrase are you looking for: ");
-        String search = input.nextLine();
-        if(quote.toLowerCase().contains(search.toLowerCase())){
-            System.out.println("Your word/phrase: " + search + " was found in the text!");
+        String searchWord = input.nextLine();
+
+        Integer index = originalQuote.indexOf(searchWord);
+        Integer length = searchWord.length();
+
+        if(originalQuote.toLowerCase().contains(searchWord.toLowerCase())){
+            System.out.println("Your word/phrase: " + searchWord + " was found in the text at index " + index + ". Your word/phrase is " + length + " characters long.");
+            String modifiedQuote = originalQuote.replace(searchWord, "");
+            System.out.println(modifiedQuote);
         }else {
-            System.out.println("Your word/phrase: " + search + " was not found in the text...");
+            System.out.println("Your word/phrase: " + searchWord + " was not found in the text...");
         }
     }
 }
