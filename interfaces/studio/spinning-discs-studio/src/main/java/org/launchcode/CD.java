68412ee4be21cac2cf46338b83c95ebf80a81869
Eric S <estriler@hotmail.com>
2024-09-26 20:38:00 -0500

    started interfaces studio

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/CD.java
@@ -1,7 +1,11 @@
 package org.launchcode;
 
-public class CD {
+public class CD implements OpticalDisc{
     // TODO: Implement your custom interface.
+    public CD(String name, boolean isMusicCD, int spinRate, String format){
+        this.name = name;
+
+    }
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
